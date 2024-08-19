6434c387ad6e6d44f8ceb377e826d988a61892fa
Harish Kumar <harish.pra22@gmail.com>
2024-08-19 14:56:56 +0530

    style: Add prettier config and eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -10,11 +12,32 @@ export default createConfigForNuxt({
     stylistic: true,
   },
   dirs: {
-    src: [
-      './playground',
-    ],
+    src: ['./playground'],
   },
-})
-  .append(
-    // your custom flat config here...
-  )
+}).append({
+  plugins: {
+    prettier: prettierPlugin,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@stylistic/semi': ['error', 'always'],
+    '@stylistic/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+        multilineDetection: 'brackets',
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    ...prettierConfig.rules, // Ensure Prettier rules are applied correctly
+  },
+});
