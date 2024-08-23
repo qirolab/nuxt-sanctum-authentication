be898e87e45ba1f6b63b2253c0d60c9358edb1d0
Harish Kumar <harish.pra22@gmail.com>
2024-08-24 00:52:35 +0530

    feat: add refreshUser function to useSanctumAuth

--- src/runtime/composables/useSanctumAuth.ts
+++ src/runtime/composables/useSanctumAuth.ts
@@ -1,16 +1,27 @@
 import { computed } from 'vue';
 import { useSanctumUser } from './useSanctumUser';
 import { getOptions } from '../helpers';
+import { useSanctumFetch } from './useSanctumFetch';
 
 export const useSanctumAuth = <T>() => {
+  const options = getOptions();
+
+  // Initialize fetch client and user state
+  const client = useSanctumFetch();
   const user = useSanctumUser<T>();
 
   // Computed property for login status
   const isLoggedIn = computed(() => user.value !== null);
 
+  // Refresh user function
+  async function refreshUser() {
+    user.value = await client<T>(options.sanctumEndpoints.user);
+  }
+
   return {
     config: getOptions(),
     user,
     isLoggedIn,
+    refreshUser,
   };
 };
