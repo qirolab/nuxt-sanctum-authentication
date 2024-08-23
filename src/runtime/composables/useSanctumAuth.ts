e577ec6385aeeb756e69ea8ca7f9c287fba77ecf
Harish Kumar <harish.pra22@gmail.com>
2024-08-24 00:54:43 +0530

    feat: add login function to useSanctumAuth

--- src/runtime/composables/useSanctumAuth.ts
+++ src/runtime/composables/useSanctumAuth.ts
@@ -1,10 +1,13 @@
 import { computed } from 'vue';
 import { useSanctumUser } from './useSanctumUser';
-import { getOptions } from '../helpers';
+import { getOptions, trimTrailingSlash } from '../helpers';
 import { useSanctumFetch } from './useSanctumFetch';
+import { navigateTo, useNuxtApp, useRoute } from '#app';
+import { useTokenStorage } from './useTokenStorage';
 
 export const useSanctumAuth = <T>() => {
   const options = getOptions();
+  const nuxtApp = useNuxtApp();
 
   // Initialize fetch client and user state
   const client = useSanctumFetch();
@@ -18,10 +21,50 @@ export const useSanctumAuth = <T>() => {
     user.value = await client<T>(options.sanctumEndpoints.user);
   }
 
+  // Login function
+  async function login(credentials: Record<string, any>) {
+    const currentRoute = useRoute();
+    const currentPath = trimTrailingSlash(currentRoute.path);
+
+    const response = await client(options.sanctumEndpoints.login, {
+      method: 'post',
+      body: credentials,
+    });
+
+    if (options.authMode === 'token') {
+      await useTokenStorage().set(response.token);
+    }
+
+    await refreshUser();
+
+    if (options.redirect.enableIntendedRedirect) {
+      const requestedRoute = currentRoute.query.redirect;
+      if (requestedRoute && requestedRoute !== currentPath) {
+        await nuxtApp.runWithContext(
+          async () => await navigateTo(requestedRoute as string),
+        );
+        return;
+      }
+    }
+
+    if (
+      !options.redirect.redirectToAfterLogin ||
+      currentRoute.path === options.redirect.redirectToAfterLogin
+    ) {
+      return;
+    }
+
+    await useNuxtApp().runWithContext(
+      async () =>
+        await navigateTo(options.redirect.redirectToAfterLogin as string),
+    );
+  }
+
   return {
     config: getOptions(),
     user,
     isLoggedIn,
     refreshUser,
+    login,
   };
 };
