9ed6b49ba2e370800974b83d7d9e298beee7b2fd
Harish Kumar <harish.pra22@gmail.com>
2024-08-24 14:13:59 +0530

    feat: add sanctum auth config

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -2,4 +2,12 @@ import MyModule from '../../../src/module';
 
 export default defineNuxtConfig({
   modules: [MyModule],
+
+  runtimeConfig: {
+    public: {
+      sanctumAuth: {
+        apiUrl: 'http://localhost:80',
+      },
+    },
+  },
 });
