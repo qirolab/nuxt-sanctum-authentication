00f475b422e4b7a8b6ec4f212bcc1920c5a92bbd
Harish Kumar <harish.pra22@gmail.com>
2024-08-25 19:34:31 +0530

    feat: add intended redirect for guest middleware

--- src/runtime/middleware/sanctum.guest.ts
+++ src/runtime/middleware/sanctum.guest.ts
@@ -1,16 +1,32 @@
 import { useSanctumAuth } from '../composables/useSanctumAuth';
-import { getOptions } from '../helpers';
-import { defineNuxtRouteMiddleware, navigateTo, createError } from '#app';
+import { getOptions, trimTrailingSlash } from '../helpers';
+import {
+  defineNuxtRouteMiddleware,
+  navigateTo,
+  createError,
+  useRoute,
+} from '#app';
 
 export default defineNuxtRouteMiddleware(async (_to, _from) => {
-  const options = getOptions();
   const { isLoggedIn } = useSanctumAuth();
+  const options = getOptions();
 
-  if (isLoggedIn.value && options.redirect.guestOnlyRedirect) {
+  if (!isLoggedIn.value) return;
+
+  if (options.redirect.enableIntendedRedirect) {
+    const currentRoute = useRoute();
+    const currentPath = trimTrailingSlash(currentRoute.path);
+
+    const requestedRoute = currentRoute.query.redirect as string;
+
+    if (requestedRoute && requestedRoute !== currentPath) {
+      return navigateTo(requestedRoute);
+    }
+  }
+
+  if (options.redirect.guestOnlyRedirect) {
     return navigateTo(options.redirect.guestOnlyRedirect, { replace: true });
   }
 
-  if (isLoggedIn.value) {
-    throw createError({ statusCode: 403 });
-  }
+  throw createError({ statusCode: 403 });
 });
