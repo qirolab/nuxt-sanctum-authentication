389e8c42a6d0c4d0aab5168b60b2cfbc1678b65a
Harish Kumar <harish.pra22@gmail.com>
2024-08-28 15:53:50 +0530

    feat: update sanctum auth module

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,12 +1,31 @@
 export default defineNuxtConfig({
-  ssr: true,
   modules: ['../src/module'],
-  devtools: { enabled: false },
+  devtools: { enabled: true },
+
   devServer: {
     host: 'laravel-api.test',
   },
+
   sanctumAuth: {
     apiUrl: 'http://laravel-api.test',
+    authMode: 'cookie',
+    userStateKey: 'sanctum.authenticated.user',
+    tokenStorageKey: 'AUTH_TOKEN',
+    fetchClientOptions: {
+      retryAttempts: false,
+    },
+    csrf: {
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'X-XSRF-TOKEN',
+    },
+    sanctumEndpoints: {
+      csrf: '/sanctum/csrf-cookie',
+      login: '/login',
+      // login: '/api/sanctum/token',
+      logout: '/logout',
+      // logout: '/api/sanctum/token/delete',
+      user: '/api/user',
+    },
     redirect: {
       enableIntendedRedirect: true,
       loginPath: '/auth/login',
@@ -14,5 +33,12 @@ export default defineNuxtConfig({
       redirectToAfterLogin: '/dashboard',
       redirectToAfterLogout: '/auth/login',
     },
+    middlewareNames: {
+      auth: '$auth',
+      guest: '$guest',
+    },
+    logLevel: 3,
   },
+
+  compatibilityDate: '2024-08-26',
 });
