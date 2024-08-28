53c057b150e3a7df8a1a4e9aca594cc91fae7bc8
Harish Kumar <harish.pra22@gmail.com>
2024-08-28 16:08:23 +0530

    feat: add useAuthUser composable

---
+++ src/runtime/composables/useAuthUser.ts
@@ -0,0 +1,16 @@
+import type { Ref } from 'vue';
+import { useSanctumOptions } from './useSanctumOptions';
+import { useState } from '#app';
+
+/**
+ * Returns a ref to the current Authenticated user.
+ *
+ * @template T The type of the user object.
+ * @returns {Ref<T | null>} A ref to the current user.
+ */
+export const useAuthUser = <T>(): Ref<T | null> => {
+  const { userStateKey } = useSanctumOptions();
+  const user = useState<T | null>(userStateKey, () => null);
+
+  return user;
+};
