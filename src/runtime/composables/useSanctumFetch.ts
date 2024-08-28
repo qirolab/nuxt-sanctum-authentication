a1566608e3368ac2b500dbd767ce5788c12e62eb
Harish Kumar <harish.pra22@gmail.com>
2024-08-28 17:06:44 +0530

    feat: add generic type parameters to useSanctumFetch

--- src/runtime/composables/useSanctumFetch.ts
+++ src/runtime/composables/useSanctumFetch.ts
@@ -1,8 +1,16 @@
-import type { $Fetch } from 'ofetch';
+import type {
+  $Fetch,
+  FetchRequest,
+  FetchOptions,
+  MappedResponseType,
+  ResponseType,
+} from 'ofetch';
 import { useNuxtApp } from '#app';
 
-export const useSanctumFetch = (): $Fetch => {
+export const useSanctumFetch = <T = any, R extends ResponseType = 'json'>(
+  request: FetchRequest,
+  options?: FetchOptions<R>,
+): Promise<MappedResponseType<R, T>> => {
   const { $sanctumFetch } = useNuxtApp();
-
-  return $sanctumFetch as $Fetch;
+  return ($sanctumFetch as $Fetch)<T, R>(request, options);
 };
