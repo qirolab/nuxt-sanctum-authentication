e17a0c525eb84acfe3e57207c59b6918f12bf0bd
Harish Kumar <harish.pra22@gmail.com>
2024-08-28 16:03:16 +0530

    feat: display user data in home page

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,13 +1,19 @@
+<script lang="ts" setup>
+const { user } = useSanctumAuth();
+</script>
+
 <!-- eslint-disable vue/singleline-html-element-content-newline -->
 <template>
   <div
     style="
       display: flex;
+      flex-direction: column;
       justify-content: center;
       align-items: center;
       margin-top: 20px;
     "
   >
     <h1 style="text-align: center">Home Page</h1>
+    <pre v-if="user">{{ user }}</pre>
   </div>
 </template>
