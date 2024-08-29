a53cc7cd054f22634c6cab6a00693766a8ee0096
Harish Kumar <harish.pra22@gmail.com>
2024-08-29 20:13:49 +0530

    fix: use correct module config key

--- src/runtime/composables/useSanctumOptions.ts
+++ src/runtime/composables/useSanctumOptions.ts
@@ -1,4 +1,5 @@
 import type { ModuleOptions } from '../types/ModuleOptions';
+import { MODULE_CONFIG_KEY } from '../helpers/module-info';
 import { useRuntimeConfig } from '#app';
 
 /**
@@ -7,5 +8,5 @@ import { useRuntimeConfig } from '#app';
  * @returns {ModuleOptions} The Sanctum module options.
  */
 export const useSanctumOptions = (): ModuleOptions => {
-  return useRuntimeConfig().public.sanctum as ModuleOptions;
+  return useRuntimeConfig().public[MODULE_CONFIG_KEY] as ModuleOptions;
 };
