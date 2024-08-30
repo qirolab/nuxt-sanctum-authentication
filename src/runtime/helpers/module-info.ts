9674fa087749205758f27426856a637504f13ef0
Harish Kumar <harish.pra22@gmail.com>
2024-08-30 22:51:15 +0530

    feat: add module name

--- src/runtime/helpers/module-info.ts
+++ src/runtime/helpers/module-info.ts
@@ -1,5 +1,2 @@
-import { name as ModuleName, version } from '../../../package.json';
-
-export const MODULE_NAME = ModuleName;
-export const MODULE_VERSION = version;
+export const MODULE_NAME = 'nuxt-sanctum-authentication';
 export const MODULE_CONFIG_KEY = 'sanctumAuth';
