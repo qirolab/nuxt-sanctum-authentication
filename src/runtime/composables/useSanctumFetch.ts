8f3731f1cf15a98425ef01d62c93585f71c4f666
Harish Kumar <harish.pra22@gmail.com>
2024-10-20 23:56:10 +0530

    feat: replace $sanctumFetch with a custom fetch service

--- src/runtime/composables/useSanctumFetch.ts
+++ src/runtime/composables/useSanctumFetch.ts
@@ -5,12 +5,32 @@ import type {
   MappedResponseType,
   ResponseType,
 } from 'ofetch';
-import { useNuxtApp } from '#app';
+import createFetchService from '../services/createFetchService';
+import { createLogger } from '../helpers/createLogger';
+import { useSanctumOptions } from './useSanctumOptions';
+// import { useNuxtApp } from '#app';
 
 export const useSanctumFetch = <T = any, R extends ResponseType = 'json'>(
   request: FetchRequest,
   options?: FetchOptions<R>,
 ): Promise<MappedResponseType<R, T>> => {
-  const { $sanctumFetch } = useNuxtApp();
-  return ($sanctumFetch as $Fetch)<T, R>(request, options);
+  // const { $sanctumFetch } = useNuxtApp();
+  // return ($sanctumFetch as $Fetch)<T, R>(request, options);
+
+  const moduleOptions = useSanctumOptions();
+  const logger = createLogger(moduleOptions.logLevel);
+  const serviceOptions: FetchOptions = {};
+
+  if (options?.onRequest) {
+    serviceOptions['onRequest'] = options.onRequest;
+    delete options.onRequest;
+  }
+  if (options?.onResponseError) {
+    serviceOptions['onResponseError'] = options.onResponseError;
+    delete options.onResponseError;
+  }
+
+  const fetchService: $Fetch = createFetchService(serviceOptions, logger);
+
+  return fetchService(request, options);
 };
