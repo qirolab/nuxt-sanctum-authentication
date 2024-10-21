600dafc6cdee7fffa3a53ebace124cec0c505c1f
Harish Kumar <harish.pra22@gmail.com>
2024-10-22 01:24:14 +0530

    feat: add avatar to User model

--- test/fixtures/laravel-api/app/Models/User.php
+++ test/fixtures/laravel-api/app/Models/User.php
@@ -9,7 +9,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -19,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'avatar',
         'password',
     ];
 
